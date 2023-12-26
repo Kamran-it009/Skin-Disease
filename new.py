8d32be12b36554d97963c320def396468fa46690
Muhammad Kamran Khan <109745004+Kamran-it009@users.noreply.github.com>
2023-12-26 13:01:18 +0500

    Add files via upload

---
+++ new.py
@@ -0,0 +1,8 @@
+import io
+import numpy as np
+from PIL import Image
+import streamlit as st
+from inference import predictor
+from streamlit_option_menu import option_menu
+
+st.write('Hello Streamlit')
